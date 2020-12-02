407523b7d22f3eea47992072dcfd24a146e8fce0
Chirag Patel <patel.chirag.rd@gmail.com>
2020-12-02 16:56:43 +0530

    Filter popup added

--- app/src/main/java/com/soboft/propertybroker/ui/fragments/AllPropertyListFragment.kt
+++ app/src/main/java/com/soboft/propertybroker/ui/fragments/AllPropertyListFragment.kt
@@ -1,10 +1,14 @@
 package com.soboft.propertybroker.ui.fragments
 
+import android.app.Dialog
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.slider.RangeSlider
 import com.soboft.propertybroker.R
 import com.soboft.propertybroker.adapters.AllPropertyListAdapter
 import com.soboft.propertybroker.databinding.FragmentAllPropertyListBinding
@@ -12,14 +16,14 @@ import com.soboft.propertybroker.model.PropertyListModel
 import com.soboft.propertybroker.ui.activities.PropertyDetail
 import com.soboft.propertybroker.utils.Params
 import com.soboft.properybroker.listeners.OnPropertyClick
+import java.text.NumberFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 class AllPropertyListFragment : Fragment(R.layout.fragment_all_property_list), OnPropertyClick {
 
     private var _binding: FragmentAllPropertyListBinding? = null
     private val binding get() = _binding!!
-
-    internal var dropDownValue = arrayOf("Sale", "Rent")
-    private lateinit var dropDownAdapter: ArrayAdapter<String>
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -31,13 +35,49 @@ class AllPropertyListFragment : Fragment(R.layout.fragment_all_property_list), O
         list.add(PropertyListModel("Saujanya 2"))
         binding.upcomingJobs.adapter = AllPropertyListAdapter(list, this)
 
-        dropDownAdapter = object : ArrayAdapter<String>(
-            requireContext(),
-            android.R.layout.simple_list_item_1,
-            dropDownValue
-        ) {}
-        binding.filterSpinner.adapter = dropDownAdapter
+        binding.filter.setOnClickListener {
+            showFilterDialog()
+        }
 
+    }
+
+    private fun showFilterDialog() {
+        val mDialog = Dialog(requireContext(), R.style.Theme_PropertyMainBroker)
+        mDialog.setContentView(R.layout.filter_layout)
+
+        val rangeSlider = mDialog.findViewById<RangeSlider>(R.id.rangeSlider)
+        val startDate = mDialog.findViewById<TextView>(R.id.startDate)
+        val endDate = mDialog.findViewById<TextView>(R.id.endDate)
+        val filter = mDialog.findViewById<Button>(R.id.filter)
+
+        startDate.setOnClickListener {
+            val builder = MaterialDatePicker.Builder.datePicker()
+            val picker = builder.build()
+            val now = Calendar.getInstance()
+            builder.setSelection(now.timeInMillis)
+            picker.show(requireActivity().supportFragmentManager, picker.toString())
+        }
+
+        endDate.setOnClickListener {
+            val builder = MaterialDatePicker.Builder.datePicker()
+            val picker = builder.build()
+            val now = Calendar.getInstance()
+            builder.setSelection(now.timeInMillis)
+            picker.show(requireActivity().supportFragmentManager, picker.toString())
+        }
+
+        rangeSlider.setLabelFormatter { value: Float ->
+            val format = NumberFormat.getCurrencyInstance()
+            format.maximumFractionDigits = 0
+            format.currency = Currency.getInstance("INR")
+            format.format(value.toDouble())
+        }
+
+        filter.setOnClickListener {
+            mDialog.dismiss()
+        }
+
+        mDialog.show()
     }
 
     override fun onDestroyView() {
